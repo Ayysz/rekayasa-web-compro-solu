8d158e62914dbc84b3e5e41a3adbd6500b0fe4b3
Ayysz <ammar.xoc2@gmail.com>
2026-06-26 10:54:45 +0700

    feat: add secret migrate route with secret_code

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ Route::get('/', function () {
 
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 Route::get('/services', [ProductController::class, 'services'])->name('services');
+
+
+Route::get('/secret_migrate', function (Request $request) {
+    if ($request->get('code') !== env('SECRET_MIGRATE_CODE')) {
+        return "Anda tidak memiliki akses untuk mengakses halaman ini.";
+    }
+    Artisan::call('migrate --force');
+    return "Migrasi berhasil!";
+});
